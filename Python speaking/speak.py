55c4391d00ea5a3c212a63e14ebf4f672c8a6829
Deepayan Thakur <84804309+Deepayan-Thakur@users.noreply.github.com>
2021-06-08 16:22:43 +0530

    Update speak.py

--- Python speaking/speak.py
+++ Python speaking/speak.py
@@ -1,27 +1,43 @@
-import pyttsx3
+import pyttsx3                  #This is necessary to install first
 
-me = pyttsx3.init()
-rate = me.getProperty('rate')
-me.setProperty('rate', rate-50)
+#-------------------------------------------------------------------------------------------------
+
+me = pyttsx3.init()                       #
+rate = me.getProperty('rate')             # This is to make the speech little bit slow 
+me.setProperty('rate', rate-50)           # 
+
+#-------------------------------------------------------------------------------------------------
 
 intro = "Hello my name is Jarvis \
-    Please write your name below !!!"
+    Please write your name below !!!"                       #This is only for saying
 me.say(intro)
 me.runAndWait()
 
-name = input("Write your name please : ")
+#-------------------------------------------------------------------------------------------------
+
+name = input("Write your name please : ")                           # This is for the user to input name
+
+#-------------------------------------------------------------------------------------------------
 
 me.say("Nice to meet you!"+ name +" "+ "\
-    please write someting below, so that i can speak it , and also please tell me.")
+    please write someting below, so that i can speak it , and also please tell me.")                    #This is only for saying
 me.runAndWait()
+
+#-------------------------------------------------------------------------------------------------
 
 me.say("How many times you want to say me the lines: ")
-me.runAndWait()
-num = int(input("Write the number of times : "))
+me.runAndWait()                                                                     #This is only for saying
+
+#-------------------------------------------------------------------------------------------------
+
+num = int(input("Write the number of times : "))                # This is how many times you write sentences
+
+#-------------------------------------------------------------------------------------------------
 
 for i in range(num):
-    me.say("please write someting below")
+    me.say("please write someting below")                               #This is only for saying
     me.runAndWait()
     user_input = input("PLease Write >... ")
     me.say(user_input)
     me.runAndWait()
+#-------------------------------------- Thank you -----------------------------------------------------------
